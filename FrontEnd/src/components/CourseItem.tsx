1165ff6389be80a9dd5cc71e31a34d4fa0a7204a
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-02-25 18:53:08 +0530

    UI 4

--- FrontEnd/src/components/CourseItem.tsx
+++ FrontEnd/src/components/CourseItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Course } from "./../Course_Directory";
 import "./../styles/courseItem.css";
+import RadioUncheckedIcon from "./../assets/radio_button_unchecked.png";
+import CheckIcon from "./../assets/check.png";
 export default function CourseItem(props: {
 	course: Course;
 	selected: string[];
@@ -23,11 +25,20 @@ export default function CourseItem(props: {
 				props.selected.includes(props.course.Code) ? "selected" : ""
 			} `}
 		>
+			{!props.selected.includes(props.course.Code) ? (
+				<img
+					className="check-icon"
+					src={RadioUncheckedIcon}
+					alt="unchecked"
+				/>
+			) : (
+				<img className="check-icon" src={CheckIcon} alt="unchecked" />
+			)}
 			<div className="field-wrapper">
 				<span className="key">
 					<strong>Code: </strong>
 				</span>
-				<span className="value">{props.course.Code}</span>
+				<span className="value code">{props.course.Code}</span>
 			</div>
 			<div className="field-wrapper">
 				<span className="key">
@@ -39,7 +50,7 @@ export default function CourseItem(props: {
 				<span className="key">
 					<strong>Credits: </strong>
 				</span>
-				<span className="value">{props.course.Credits}</span>
+				<span className="value credits">{props.course.Credits}</span>
 			</div>
 			{props.course.Prerequisite ? (
 				<div className="field-wrapper">

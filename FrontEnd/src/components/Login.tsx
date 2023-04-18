e451188bf32ff37f70345b3bf148131ac6bc5d8f
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-04-18 19:42:28 +0530

    test

--- FrontEnd/src/components/Login.tsx
+++ FrontEnd/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+import jwt_decode from "jwt-decode";
 import { GoogleLogin } from "@react-oauth/google";
 export default function Login() {
 	return (
@@ -8,7 +8,7 @@ export default function Login() {
 			<GoogleOAuthProvider clientId="51730502551-mkkokhpvqbutmqbjsfifnhcdvghe8va9.apps.googleusercontent.com">
 				<GoogleLogin
 					onSuccess={(credentialResponse) => {
-						console.log(credentialResponse);
+						console.log(jwt_decode(credentialResponse.credential!));
 					}}
 					onError={() => {
 						console.log("Login Failed");

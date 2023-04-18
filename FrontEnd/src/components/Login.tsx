7ca2a43c824b475a650e945cca96b63439e31fe7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-04-18 20:08:15 +0530

    .

--- FrontEnd/src/components/Login.tsx
+++ FrontEnd/src/components/Login.tsx
@@ -2,14 +2,40 @@ import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
 import { GoogleLogin } from "@react-oauth/google";
+type Credentials = {
+	aud: string;
+	azp: string;
+	email: string;
+	email_verified: boolean;
+	exp: number;
+	family_name: string;
+	given_name: string;
+	hd: string;
+	iat: number;
+	iss: string;
+	jti: string;
+	name: string;
+	nbf: number;
+	picture: string;
+	sub: string;
+};
+
 export default function Login() {
 	return (
 		<div>
 			<GoogleOAuthProvider clientId="51730502551-mkkokhpvqbutmqbjsfifnhcdvghe8va9.apps.googleusercontent.com">
 				<GoogleLogin
 					onSuccess={(credentialResponse) => {
-						console.log(jwt_decode(credentialResponse.credential!));
-						//work
+						const host = (
+							jwt_decode(
+								credentialResponse.credential!
+							) as Credentials
+						).hd;
+						if (host === "ahduni.edu.in") {
+							console.log("Login Successful");
+						} else {
+							console.log("Login Failed");
+						}
 					}}
 					onError={() => {
 						console.log("Login Failed");

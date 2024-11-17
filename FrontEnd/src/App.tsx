23dfe56dffeca14d2ab1f8fcf656eaccc4456801
Vishv0407 <vishvkumar.b@ahduni.edu.in>
2024-11-17 15:51:41 +0530

    Added Selected courses tag

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { TextField } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 import CourseDirectory from "./Course_Directory";
 import winter from "./Data/winter.json";
 import monsoon from "./Data/monsoon.json";
@@ -8,26 +8,29 @@ import "./App.css";
 import CourseExplorer from "./components/CourseExplorer/CourseExplorer";
 import SemesterSwitch from "./components/SemesterSwitch/SemesterSwitch";
 import ActionBar from "./components/ActionBar/ActionBar";
+import SelectedCourses from "./components/ActionBar/SelectedCourses/SelectedCourses";
 import Combinator from "./components/Combinator/Combinator";
-import { Analytics } from "@vercel/analytics/react";
-import { Link } from "react-router-dom";
 
 export default function App() {
 	const [cd, setCD] = React.useState(new CourseDirectory(winter, monsoon));
 	const [query, setQuery] = React.useState("");
-	// console.log(cd.getScheduleFromCodeAndSection("ENR100-1"));
 	const [selected, setSelected] = React.useState<string[]>([]);
-	React.useEffect(() => {
-		localStorage.getItem("selected") &&
-			setSelected(JSON.parse(localStorage.getItem("selected")!));
-	}, []);
 	const [combinations, setCombinations] = React.useState<string[][]>([]);
+	const [isCombinatorOpen, setIsCombinatorOpen] = React.useState(false);
+
+	// Load selected courses from localStorage on mount
+	React.useEffect(() => {
+		const storedSelected = localStorage.getItem("selected");
+		if (storedSelected) {
+			setSelected(JSON.parse(storedSelected));
+		}
+	}, []);
+
+	// Update combinations and persist selected courses to localStorage
 	React.useEffect(() => {
 		setCombinations(cd.generatePossibleCombinations(selected));
 		localStorage.setItem("selected", JSON.stringify(selected));
 	}, [selected]);
-
-	const [isCombinatorOpen, setIsCombinatorOpen] = React.useState(false);
 
 	return (
 		<main>
@@ -35,9 +38,9 @@ export default function App() {
 				name="viewport"
 				content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
 			/>
-
 			<Analytics />
 			<span className="update-status">Updated on 16th Nov 2024</span>
+
 			{isCombinatorOpen ? (
 				<Combinator
 					cd={cd}
@@ -53,20 +56,20 @@ export default function App() {
 						}`}
 					>
 						<h1 className="title">Class Combinator</h1>
-						<SearchBar
-							query={query}
-							setQuery={setQuery}
-						></SearchBar>
-						<SemesterSwitch
-							query={query}
-							setQuery={setQuery}
-							cd={cd}
-							setCD={setCD}
-							setSelected={setSelected}
-						/>
+						<SearchBar query={query} setQuery={setQuery} />
+						<div className="semester-and-selected-wrapper">
+							<SemesterSwitch
+								query={query}
+								setQuery={setQuery}
+								cd={cd}
+								setCD={setCD}
+								setSelected={setSelected}
+							/>
+							<SelectedCourses selected={selected} setSelected={setSelected} />
+						</div>
 					</div>
 					<div
-						className={` ${
+						className={`${
 							query === "" ? "default-height" : "expended"
 						} course-explorer-wrapper`}
 					>

a31a3748029966dd036f71f5292d5a3c23f4698a
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-11-09 19:36:18 +0530

    add database connection handling and data insertion in the scrapper; add environment variable checks and update dependencies

--- DataScraping/push_to_db.py
+++ DataScraping/push_to_db.py
@@ -1,10 +1,33 @@
 
 
 import json
+import os
 from jsonschema import validate
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_db_variables():
+    MONGO_URI = os.getenv("MONGO_URI")
+    CLIENT_DB = os.getenv("CLIENT_DB")
+    CLIENT_COLLECTION = os.getenv("CLIENT_COLLECTION")
+
+    if CLIENT_DB is None:
+        raise ValueError("CLIENT_DB environment variable not set")
+    if CLIENT_COLLECTION is None:
+        raise ValueError("CLIENT_COLLECTION environment variable not set")
+    if MONGO_URI is None:
+        raise ValueError("MONGO_URI environment variable not set")
+    
+    return MONGO_URI, CLIENT_DB, CLIENT_COLLECTION
 
 
 def main():
+
+  MONGO_URI, CLIENT_DB, CLIENT_COLLECTION = get_db_variables()
+  
   import argparse
   import sys
   
@@ -29,7 +52,30 @@ def main():
   validate(instance=data, schema=schema)
   print("Validation successful.")
   # if validation is successful, we can push to db
+  print("Connecting to MongoDB...")
+  client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+  db = client[CLIENT_DB]
+  collection = db[CLIENT_COLLECTION]
+  print("Connection successful.")
 
   print(f"Pushing data from {json_file} to the database...")
+  # now we can push the data to db
+  skipped = 0
+  
+  with open(json_file) as f:
+      courses = json.load(f)
+      if isinstance(courses, list):
+          
+          result = collection.insert_many(courses)
+          print(f"Inserted {len(result.inserted_ids)} documents into the collection.")
+      else:
+          result = collection.insert_one(courses)
+          print(f"Inserted document with id {result.inserted_id} into the collection.")
 
-main()
+  
+  
+
+  
+if __name__ == "__main__":
+    main()
+    

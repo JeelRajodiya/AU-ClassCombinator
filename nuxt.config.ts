8865c8fe54f6ae4a829a56993b801fda8222fde1
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-11-25 15:45:29 +0530

    add g-tag and gsite config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@sidebase/nuxt-auth"],
+  modules: ["@nuxt/ui", "@sidebase/nuxt-auth", "nuxt-gtag"],
   css: ["~/assets/css/main.css"],
+  gtag: {
+    enabled: true,
+    id: process.env.NUXT_PUBLIC_GTAG_ID,
+  },
   app: {
     head: {
       title: "Class Combinator",
@@ -11,12 +15,16 @@ export default defineNuxtConfig({
         {
           name: "description",
           content:
-            "Ahmedabad University Class Combinator. Generate class timetables based on your course selections from auris data.",
+            "AU Class Combinator, schedule planner better than auris ! Find the perfect class schedule at Ahmedabad University with Class Combinator. Our easy-to-use course combinator tool generates all possible class combinations, ensuring that you can create a schedule without any clashes. Select your preferred courses and let ClassCombinator do the rest. Start planning your ideal semester today!",
         },
         {
           name: "keywords",
           content:
             "Ahmedabad University, AU, Class Combinator, Course Scheduler, Timetable Generator, Auris Directory, Course Combinations, Class Schedule Planner, Class Combinator, AU Class Combinator, ClassCombinator",
+        },
+        {
+          name: "google-site-verification",
+          content: "0U-oiurYHf7uKHEQbrNhqVIe69pMrU4FYvTA92mnvos",
         },
       ],
     },
